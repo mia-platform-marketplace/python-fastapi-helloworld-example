3a434a5c3e359e3aa9ab897c7c72bbef6f52020d
Lucas Puerari <105070316+lucas-puerari@users.noreply.github.com>
2023-07-10 14:24:40 +0200

    MCRDMPE-169 fix client headers (#3)

--- src/apis/schemas/header_schema.py
+++ src/apis/schemas/header_schema.py
@@ -6,9 +6,7 @@ class HeaderRequest(BaseModel):
     Mia Platform required headers
     """
 
-    LOG_LEVEL: str
     USERID_HEADER_KEY: str
     GROUPS_HEADER_KEY: str
     CLIENTTYPE_HEADER_KEY: str
     BACKOFFICE_HEADER_KEY: str
-    MICROSERVICE_GATEWAY_SERVICE_NAME: str
